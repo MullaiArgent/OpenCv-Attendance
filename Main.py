39b162dfd4313928b1b7abcabe273a36e43a1669
MullaiArgent <mullairajan2000@gmail.com>
2021-07-05 21:53:10 +0530

    Face detection Initiated

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import cv2
 import face_recognition
 import os
-
+import numpy
 
 path = 'Images'
 images = []
@@ -14,11 +14,12 @@ for a in mylist:
     images.append(curimg)
     images_name.append(os.path.splitext(a)[0])
 
-def findencodings(images):
+
+def findencodings(a):
     encodelist = []
-    for img in images:
-        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-        encode = face_recognition.face_encodings(img) [0]
+    for img in a:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
         encodelist.append(encode)
     print("Encoding Completed")
     return encodelist
@@ -26,10 +27,21 @@ def findencodings(images):
 
 encodelist = findencodings(images)
 
+cap = cv2.VideoCapture(0)
 
-cap  = cv2.VideoCapture(0)
-
-while(True):
+while True:
     success, img = cap.read()
+    imgs = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgs = cv2.cvtColor(imgs, cv2.COLOR_BGR2RGB)
+    faceCurframe = face_recognition.face_locations(imgs)
+    encodeCurframe = face_recognition.face_encodings(imgs, faceCurframe)
 
+    for encodeface, faceloc in zip(encodeCurframe, faceCurframe):
+        matchs = face_recognition.compare_faces(encodelist, encodeface)
+        facedis = face_recognition.face_distance(encodelist, encodeface)
 
+        matchIndex = numpy.argmin(facedis)
+
+        if matchs[matchIndex]:
+            name = images_name[matchIndex].upper()
+            print(name)
