135a59c85ce70d13b224aa9eeca79bd4613ea433
MullaiArgent <mullairajan2000@gmail.com>
2021-07-05 20:27:23 +0530

    Attendance project's, image files has been read and encoded.

--- Main.py
+++ Main.py
@@ -1,23 +1,26 @@
 import cv2
 import face_recognition
+import os
 
-imgMullai_f = face_recognition.load_image_file("TestImages/mullai_f.jpg")
-imgMullai_f = cv2.cvtColor(imgMullai_f, cv2.COLOR_BGR2RGB)
-facelocMullai = face_recognition.face_locations(imgMullai_f)[0]
-encodMullai_f = face_recognition.face_encodings(imgMullai_f)[0]
-cv2.rectangle(imgMullai_f, (facelocMullai[3], facelocMullai[0]), (facelocMullai[1], facelocMullai[2]), (0, 0, 0, 255), 3)
 
-imgMullai_t = face_recognition.load_image_file("TestImages/mullai_t.jpg")
-imgMullai_t = cv2.cvtColor(imgMullai_t, cv2.COLOR_BGR2RGB)
-facelocmullai_t = face_recognition.face_locations(imgMullai_t)[0]
-encodMullai_t = face_recognition.face_encodings(imgMullai_t)[0]
-cv2.rectangle(imgMullai_t, (facelocmullai_t[3], facelocmullai_t[0]), (facelocmullai_t[1], facelocmullai_t[2]), (0, 0, 0, 255), 3)
+path = 'Images'
+images = []
+images_name = []
 
-results = face_recognition.compare_faces([encodMullai_f],encodMullai_t)
-facedis = face_recognition.face_distance([encodMullai_f],encodMullai_t)
+mylist = os.listdir(path)
 
-cv2.putText(imgMullai_t,f'{results},{round(facedis[0],2)}',(100,100),cv2.FONT_HERSHEY_COMPLEX_SMALL,1,(0,0,255),2)
+for a in mylist:
+    curimg = cv2.imread(f'{path}/{a}')
+    images.append(curimg)
+    images_name.append(os.path.splitext(a)[0])
 
-cv2.imshow("Mullai Rajan f", imgMullai_f)
-cv2.imshow("Mullai Rajan t", imgMullai_t)
-cv2.waitKey(0)
+def findencodings(images):
+    encodelist = []
+    for img in images:
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img) [0]
+        encodelist.append(encode)
+    return encodelist
+
+
+print(findencodings(images))
